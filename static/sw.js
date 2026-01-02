750a2051824eb62b72ff732336247b782118fe0c
Fred gruber Ferreira <fredgruber@gmail.com>
2026-01-02 12:30:54 -0300

    site transformado em PWA

---
+++ static/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'invest-sim-v1';
+const urlsToCache = [
+    '/',
+    '/static/style.css',
+    '/static/manifest.json',
+    '/static/app_icon.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
